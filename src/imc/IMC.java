2f07fa8e6712cd91988c82fa9889179e98d55e26
diego programer <diegoalexac@gmail.com>
2019-09-18 19:26:36 -0500

    programa de promedio de notas con vectores en java

---
+++ src/imc/IMC.java
@@ -0,0 +1,28 @@
+/*
+PROGRAMA Q HALLA EL INDICE DE MASA CORPORAL
+ */
+package imc;
+
+/**
+ *
+ * @author alexander
+ */
+public class IMC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Ventana v1 = new Ventana();
+    // hacer visible ventana
+    v1.setVisible(true);
+    // llamado a la ventana 3
+    Ventana3 v3= new Ventana3();
+   // v3.setVisible(true);
+    // posicion
+    //v3.setLocation(40,40);
+            
+        // 
+    }
+    
+}
